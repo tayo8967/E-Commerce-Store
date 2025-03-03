f093e8b563b27667275012874f3910a080253849
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-03 23:51:18 +0800

    Add authentication middleware and featured products route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -9,3 +9,13 @@ export const getAllProducts = async (req, res) => {
         res.status(500).json({ message: "Server error:", error: error.message });
     }
 };
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featuredProducts", async (error, data) => {
+            //continue here
+        });
+    } catch (error) {
+        
+    }
+};
