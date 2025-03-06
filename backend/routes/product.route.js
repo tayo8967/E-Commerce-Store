adcca84c143552d2730086d5363ab3e09dfa3f81
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-06 18:37:22 +0800

    feature/products (#2)
    * Add product model, routes, and controller for product management.
    
    * Add authentication middleware and featured products route
    
    * Implement product management features including CRUD operations, caching with Redis, and Cloudinary integration for image uploads
    
    * Fix typo in cloudinary image deletion function

---
+++ backend/routes/product.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { getAllProducts, getFeaturedProducts, createProduct, 
+    deleteProduct, getRecommendedProducts, getProductsByCategory,
+    toggleFeaturedProduct
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.get("/recommendation", getRecommendedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
+export default router;
