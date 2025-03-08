b14090eece06a54ab187e00487615437a4e6c2f3
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-08 09:32:02 +0800

    feature/coupon: Add coupon model, controller, and routes for coupon management (#4)
    * Add coupon model, controller, and routes for coupon management
    
    * Fix typo in coupon model: corrected 'reqquired' to 'required'

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started on http://localhost:" + PORT);
