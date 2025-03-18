b4283e33d8f599a57c40ad0084d6af6719a6ab76
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-18 23:21:58 +0800

    feature/auth: Enhance authentication flow with loading state and redirects

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,9 +2,9 @@ import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from 'framer-motion';
+import { useUserStore } from '../stores/useUserStore';
 
 const SignUpPage = () => {
-  const loading = false;
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -12,9 +12,11 @@ const SignUpPage = () => {
     confirmPassword: ''
   });
 
+const { signup, loading } =  useUserStore();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    signup(formData);
   };
 
   return (
