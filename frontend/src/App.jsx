348c1772aacc8c6ec429390b5b1d7ba365d8eb97
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-22 22:41:17 +0800

    feature/category: Implement category page with product listing and fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
-import { Toaster } from 'react-hot-toast';
+import AdminPage from './pages/AdminPage';
+import CategoryPage from './pages/CategoryPage';
 
 import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
 import { useUserStore } from './stores/useUserStore';
 import { useEffect } from 'react';
 import LoadingSpinner from './components/LoadingSpinner';
-import AdminPage from './pages/AdminPage';
+
 
 function App() {
   const { user, checkAuth, checkingAuth } = useUserStore();
@@ -34,7 +37,12 @@ function App() {
           <Route path='/' element= {<HomePage />} />
           <Route path='/signup' element= {!user ? <SignUpPage /> : <Navigate to='/' />} />
           <Route path='/login' element= {!user ? <LoginPage /> : <Navigate to='/' />} />
-          <Route path='/secret-dashboard' element= {user?.role === 'admin' ? <AdminPage /> : <Navigate to='/login' />} />
+          <Route 
+            path='/secret-dashboard' 
+            element= {user?.role === 'admin' ? <AdminPage /> : <Navigate to='/login' />} />
+          <Route 
+            path='/category/:category' 
+            element= {<CategoryPage />} />
         </Routes>
       </div>
       <Toaster />
