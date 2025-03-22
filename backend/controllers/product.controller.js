ad29ac9a5d8903c35061e295370152f32e34c5ae
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-22 19:15:26 +0800

    feature/admin: Implement admin dashboard (#11)
    * Add admin dashboard with product creation and analytics tabs
    
    * Enhance product creation with image upload and state management
    
    * Enhance admin page with product fetching and improved product list display

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,10 +1,11 @@
 import Product from "../models/product.model.js";
 import cloudinary from "../lib/cloudinary.js";
+import { redis } from "../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
     try {
         const products = await Product.find({});
-        res.json(products);
+        res.json({ products });
     } catch (error) {
         console.log("Error in getAllProducts controller:", error.message);
         res.status(500).json({ message: "Server error:", error: error.message });
@@ -43,19 +44,19 @@ export const createProduct = async (req, res) => {
     try {
         const {name, description, price, image, category} = req.body;
 
-        let cloudinaryRespone = null;
+        let cloudinaryResponse = null;
 
         if (image) {
-            cloudinaryRespone = await cloudinary.uploader.upload(image, {folder: "products"});
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: "products"});
         }
 
-        const product = new Product({
+        const product = await Product.create({
             name,
             description,
             price,
             image: cloudinaryResponse.secure_url ? cloudinaryResponse.secure_url : "",
             category
-        })
+        });
 
         res.status(201).json(product);
     } catch (error) {
@@ -152,8 +153,8 @@ async function updateFeaturedProductsCache() {
     try {
         // The lean() method is used to return a plain JavaScript object instead of a MongoDB document
 
-        const featureProducts = await Product.find({ isFeatured: true }).lean();
-        await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+        const featuredProducts = await Product.find({ isFeatured: true }).lean();
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
     } catch (error) {
         console.log("Error updating featured products cache:", error.message);
     }
