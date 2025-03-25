15454bcfacd346f593c50e92e369a1c699af031e
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-25 22:28:09 +0800

    feature/cart: Implement cart (#13)
    * feature/cart: Implement cart functionality with add to cart feature
    
    * Add cart page and cart item component with functionality to update quantity and remove items
    
    * Add gift coupon functionality and order summary to cart page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,15 +1,17 @@
 import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductCard = ({ product }) => {
     const { user } = useUserStore();
+    const { addToCart } = useCartStore();
     const handleAddToCart = () => {
         if (!user) {
             toast.error("Please login to add products to cart", { id: "login" });
             return;
         } else {
-            toast.success("Added to cart",  { id: "added" });
+            addToCart(product);
         }
     };
 
