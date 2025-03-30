07cace79299ae432374130a685e0a49c5ca89b5f
Levin Cabalse <74039490+tayo8967@users.noreply.github.com>
2025-03-30 13:27:52 +0800

    feature: Add Featured Products component and integrate with product store (#18)

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -14,26 +14,25 @@ export const getAllProducts = async (req, res) => {
 
 export const getFeaturedProducts = async (req, res) => {
     try {
-        let featuredProducts = await redis.get("featuredProducts", async (error, data) => {
-            if (featuredProducts) {
-                return res.json(JSON.parse(featuredProducts));
-            }
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
 
-            // if not in redis, fetch from mongodb
-            // .lean() is used to return a plain javascript object instead of a mongodb document
-            // which is good for performance
-            featuredProducts = await Product.find({ isFeatured: true }).lean();
+        // if not in redis, fetch from mongodb
+        // .lean() is used to return a plain javascript object instead of a mongodb document
+        // which is good for performance
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
 
-            if (!featuredProducts) {
-                return res.status(404).json({ message: "No featured products found" });
-            }
+        if (!featuredProducts) {
+            return res.status(404).json({ message: "No featured products found" });
+        }
 
-            // store in redis for future quick access
+        // store in redis for future quick access
 
-            await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
 
-            res.json(featuredProducts);
-        });
+        res.json(featuredProducts);
     } catch (error) {
         console.log("Error in getFeaturedProducts controller:", error.message);
         res.status(500).json({ message: "Server error:", error: error.message });
